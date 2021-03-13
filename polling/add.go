c98a1bc8d6cf01656cfb6fcb5b493466b3f8a464
EC2 Default User <ec2-user@ip-172-31-5-56.us-east-2.compute.internal>
2021-03-13 16:58:41 +0000

    Fixing a bug with add crashing if no space

--- polling/add.go
+++ polling/add.go
@@ -6,14 +6,17 @@ import (
 )
 
 func addOption(poll *Poll, session *discordgo.Session, message *discordgo.MessageCreate) {
-	var emote string = (*poll).emotes[poll.lastLetter] //Pull from dictionary
-	(*poll).lastLetter++
-
 	var split = strings.Split(message.Content, ": ")
-	(*poll).entries[emote] = split[1]
-	(*poll).entriesReverse[split[1]] = emote
-	(*poll).pollMessage.Content = (*poll).pollMessage.Content + "\n" + split[1] + ": " + emote + "\n"
 
-	_, _ = session.ChannelMessageEdit(message.ChannelID, (*poll).pollMessage.ID, (*poll).pollMessage.Content)
-	go session.MessageReactionAdd(message.ChannelID, (*poll).pollMessage.ID, emote)
+	if len(split) > 1 {
+		var emote string = (*poll).emotes[poll.lastLetter] //Pull from dictionary
+		(*poll).lastLetter++
+
+		(*poll).entries[emote] = split[1]
+		(*poll).entriesReverse[split[1]] = emote
+		(*poll).pollMessage.Content = (*poll).pollMessage.Content + "\n" + split[1] + ": " + emote + "\n"
+
+		_, _ = session.ChannelMessageEdit(message.ChannelID, (*poll).pollMessage.ID, (*poll).pollMessage.Content)
+		go session.MessageReactionAdd(message.ChannelID, (*poll).pollMessage.ID, emote)
+	}
 }
