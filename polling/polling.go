364d069997b7f4def33270aac06061340cbcd52f
EC2 Default User <ec2-user@ip-172-31-5-56.us-east-2.compute.internal>
2021-03-14 00:40:50 +0000

    Only delete commands

--- polling/polling.go
+++ polling/polling.go
@@ -49,42 +49,43 @@ func messageHandler(session *discordgo.Session, message *discordgo.MessageCreate
 		}
 
 		if strings.HasPrefix(message.Content, config.BotPrefix+"start") {
-			pollByChannel[message.ChannelID] = start(session, message)
-		}
 
-		if strings.HasPrefix(message.Content, config.BotPrefix+"add:") {
+			pollByChannel[message.ChannelID] = start(session, message)
+
+		} else if strings.HasPrefix(message.Content, config.BotPrefix+"add:") {
+
 			//Doesn't let you pass an address for some god forsaken reason, so temp variable workaround
 			temp := pollByChannel[message.ChannelID]
 			addOption(&(temp), session, message)
 			pollByChannel[message.ChannelID] = temp
-		}
 
-		if strings.HasPrefix(message.Content, config.BotPrefix+"remove:") {
+		} else if strings.HasPrefix(message.Content, config.BotPrefix+"remove:") {
+
 			//Doesn't let you pass an address for some god forsaken reason, so temp variable workaround
 			temp := pollByChannel[message.ChannelID]
 			removeOption(&(temp), session, message)
 			pollByChannel[message.ChannelID] = temp
-		}
 
-		if message.Content == config.BotPrefix+"repin" {
+		} else if message.Content == config.BotPrefix+"repin" {
+
 			pin(pollByChannel[message.ChannelID], session)
-		}
 
-		if message.Content == config.BotPrefix+"view" {
+		} else if message.Content == config.BotPrefix+"view" {
+
 			view(pollByChannel[message.ChannelID], session)
-		}
 
-		if message.Content == config.BotPrefix+"help" || message.Content == config.BotPrefix+"elp" {
+		} else if message.Content == config.BotPrefix+"help" || message.Content == config.BotPrefix+"elp" {
+
 			help(session, message)
-		}
 
-		if strings.HasPrefix(message.Content, config.BotPrefix+"result") {
+		} else if strings.HasPrefix(message.Content, config.BotPrefix+"result") {
+
 			var poll = pollByChannel[message.ChannelID]
 			var res = getResult(poll, session)
 			_, _ = session.ChannelMessageSend(poll.channel, res)
-		}
 
-		if strings.HasPrefix(message.Content, config.BotPrefix+"reset") {
+		} else if strings.HasPrefix(message.Content, config.BotPrefix+"reset") {
+
 			var poll = pollByChannel[message.ChannelID]
 			var newPoll = poll
 			var split = strings.Split(message.Content, " ")
@@ -96,21 +97,24 @@ func messageHandler(session *discordgo.Session, message *discordgo.MessageCreate
 				newPoll = resetCarryOver(poll, session, message, split[1])
 			}
 			pollByChannel[message.ChannelID] = newPoll
-		}
 
-		if message.Content == config.BotPrefix+"bad" {
+		} else if message.Content == config.BotPrefix+"bad" {
+
 			bad(session, message)
-		}
 
-		if message.Content == config.BotPrefix+"runoff" {
+		} else if message.Content == config.BotPrefix+"runoff" {
+
 			var poll = pollByChannel[message.ChannelID]
 			runoff(poll, session, message)
-		}
 
-		if message.Content == config.BotPrefix+"runoffResult" {
+		} else if message.Content == config.BotPrefix+"runoffResult" {
+
 			var poll = pollByChannel[message.ChannelID]
 			var res = runoffRes(poll, session)
 			_, _ = session.ChannelMessageSend(message.ChannelID, res)
+
+		} else {
+			return
 		}
 
 		session.ChannelMessageDelete(message.ChannelID, message.ID)
