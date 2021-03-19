fbab62d18e6ab162303f1d2f3065111334bfb0f5
EC2 Default User <ec2-user@ip-172-31-5-56.us-east-2.compute.internal>
2021-03-19 03:34:21 +0000

    Fixing bug in help text

--- polling/help.go
+++ polling/help.go
@@ -3,6 +3,7 @@ package polling
 import (
 	"github.com/MisterMagnificient/DiscordPollingBotGo/config"
 	"github.com/bwmarrin/discordgo"
+	"strconv"
 	"strings"
 )
 
@@ -10,7 +11,8 @@ func help(session *discordgo.Session, channelID string, content string) {
 	index := strings.IndexByte(content, ' ')
 
 	if index == -1 {
-		_, _ = session.ChannelMessageSend(channelID, "Version 1.0.0 Beta \n\nHi, here to help with whatever you need! I can make one live poll per channel at a time.\nCOMMANDS:\n\nAll commands start with: '"+config.BotPrefix+"' \n'***start XXX***' command to start a poll with XXX question, just start will add a default question, \n'***add XXX***' command to add an option called XXX (Multiple separated by ;, i.e. XXX;YYY;ZZZ), \n'***result***' to calculate winner, \n'***view***' to get a link to the poll message\n'***reset***' to reset the poll and start a new one with all entries (that have more than "+string(config.MinCarryOver)+" votes) but the winner from the last one.   If you want a full reset, '***reset all***'\n\n '***help advanced***' for more commands! https://64.media.tumblr.com/daf862b49b82e49a47354b14c5143363/tumblr_oefwev7pLA1vvi3bvo8_250.gif")
+		carry := strconv.Itoa(config.MinCarryOver)
+		_, _ = session.ChannelMessageSend(channelID, "Version 1.0.0 Beta \n\nHi, here to help with whatever you need! I can make one live poll per channel at a time.\nCOMMANDS:\n\nAll commands start with: '"+config.BotPrefix+"' \n'***start XXX***' command to start a poll with XXX question, just start will add a default question, \n'***add XXX***' command to add an option called XXX (Multiple separated by ;, i.e. XXX;YYY;ZZZ), \n'***result***' to calculate winner, \n'***view***' to get a link to the poll message\n'***reset***' to reset the poll and start a new one with all entries (that have more than "+carry+" votes) but the winner from the last one.   If you want a full reset, '***reset all***'\n\n '***help advanced***' for more commands! https://64.media.tumblr.com/daf862b49b82e49a47354b14c5143363/tumblr_oefwev7pLA1vvi3bvo8_250.gif")
 	} else {
 		chars := []rune(content)
 		option := string(chars[index+1:])
