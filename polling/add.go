df3df101ba5cdb40440d84eca67e72d5978da600
EC2 Default User <ec2-user@ip-172-31-5-56.us-east-2.compute.internal>
2021-04-03 02:42:41 +0000

    Readjusting add message with logging

--- polling/add.go
+++ polling/add.go
@@ -16,7 +16,7 @@ func addOption(poll *Poll, session *discordgo.Session, channelID string, content
 
 		if len(split) > 0 {
 			for index := 0; index < len(split); index++ {
-				addHelper(poll, session, channelID, strings.TrimSpace(split[index]))
+				addHelper(poll, session, channelID, strings.TrimSpace(split[index]), author)
 			}
 		}
 	}
@@ -30,13 +30,16 @@ func addOption(poll *Poll, session *discordgo.Session, channelID string, content
 	}
 }
 
-func addHelper(poll *Poll, session *discordgo.Session, channelID string, element string) {
+func addHelper(poll *Poll, session *discordgo.Session, channelID string, element string, author *discordgo.User) {
 	var emote string = (*poll).Emotes[poll.LastLetter] //Pull from dictionary
 	(*poll).LastLetter++
 
 	(*poll).Entries[emote] = element
 	(*poll).EntriesReverse[element] = emote
 	(*poll).PollMessage.Content = (*poll).PollMessage.Content + "\n" + element + ": " + emote + "\n"
+	if config.LogAdd {
+		(*poll).PollMessage.Content = (*poll).PollMessage.Content + " [Nominated by " + author.Username + "]\n"
+	}
 
 	_, _ = session.ChannelMessageEdit(channelID, (*poll).PollMessage.ID, (*poll).PollMessage.Content)
 	go session.MessageReactionAdd(channelID, (*poll).PollMessage.ID, emote)
